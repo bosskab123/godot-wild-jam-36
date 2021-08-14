60b64fe2a2c5e06e271e9b18046f61e37aba0c71
bosskab123 <phatarapran7524@gmail.com>
2021-08-14 18:24:57 +0700

    [WIP] implement apple

--- scenes/obstacles/Mud.gd
+++ scenes/obstacles/Mud.gd
@@ -3,7 +3,7 @@ extends Area2D
 export(float) var SLOW_X_FACTOR = 0.6
 export(float) var SLOW_Y_FACTOR = 0.95
 var player_on_mud: bool = false
-onready var player = get_tree().get_root().find_node("Player",true,false)
+onready var player = get_tree().get_root().find_node("Player",true,false) as Player
 
 func _on_Mud_body_entered(body):
 	if body.name == "Player":
@@ -15,6 +15,6 @@ func _on_Mud_body_exited(body):
 
 func _process(delta):
 	if player_on_mud == true:
-		if player != null:
+		if player:
 			player.move_vector.x *= SLOW_X_FACTOR
 			player.move_vector.y *= SLOW_Y_FACTOR
--- scenes/obstacles/Spring.gd
+++ scenes/obstacles/Spring.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 export(float) var SPRING_FORCE: float = 1000
-onready var player = get_tree().get_root().find_node("Player",true,false)
+onready var player = get_tree().get_root().find_node("Player",true,false) as Player
 
 func _on_Spring_body_entered(body):
-	if player != null:
+	if player:
 		player.move_vector += Vector2.UP * SPRING_FORCE
