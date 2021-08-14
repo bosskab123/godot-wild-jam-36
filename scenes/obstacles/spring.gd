892b96413d2173789162fd0856fa055cb4e473b2
bosskab123 <phatarapran7524@gmail.com>
2021-08-14 16:26:30 +0700

    [WIP] implement spring

--- scenes/obstacles/spring.gd
+++ scenes/obstacles/spring.gd
@@ -1,8 +1,6 @@
 extends Area2D
 
-signal player_on_spring
-
-export(float) var SPRING_FORCE: float = 10
+export(float) var SPRING_FORCE: float = 50
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,10 +9,8 @@ export(float) var SPRING_FORCE: float = 10
 func _ready():
 	pass # Replace with function body.
 
-func _on_Area2D_body_entered(body):
-	emit_signal("player_on_pat")
-	
-	var player = get_tree().get_root().get_node("Player")
+func _on_Spring_body_entered(body):
+	var player = get_node("../Player")
 	
 	if player != null:
 		player.move_vector += Vector2.UP * SPRING_FORCE
