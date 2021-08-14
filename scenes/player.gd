7eed3826eeeedc8d6a9f523d4aeb51cbfffd959d
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2021-08-14 16:58:07 +0700

    implement jump on player

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,18 +2,22 @@ extends KinematicBody2D
 
 var move_vector: Vector2
 export(float) var MOVE_SPEED: float = 100
+export(float) var JUMP_SPEED: float = 300
 export(float) var MAX_X_SPEED: float = 300
 export(float) var MAX_Y_SPEED: float = 400
 
 func _process(delta):
 	if Input.is_action_pressed("move_right"):
-		move_vector += Vector2.RIGHT * MOVE_SPEED
+		move_vector.x += MOVE_SPEED
 	if Input.is_action_pressed("move_left"):
-		move_vector += Vector2.LEFT * MOVE_SPEED
+		move_vector.x -= MOVE_SPEED
 	
 	if !Input.is_action_pressed("move_left") and !Input.is_action_pressed("move_right"):
 		move_vector.x = lerp(move_vector.x, 0, 0.8)
-
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		move_vector.y = -JUMP_SPEED
+	
 func _physics_process(delta):
 	move_vector.y += GlobalVars.GRAVITY
 	move_vector.x = clamp(move_vector.x, -MAX_X_SPEED, MAX_X_SPEED)
