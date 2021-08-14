bed6ae8441485c8eef642f516a1ef7674370f4f5
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2021-08-14 16:31:34 +0700

    add gravity to player

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,10 +2,8 @@ extends KinematicBody2D
 
 var move_vector: Vector2
 export(float) var MOVE_SPEED: float = 10
-export(float) var MAX_MOVE_SPEED: float = 50
-
-func _ready():
-	pass
+export(float) var MAX_X_SPEED: float = 50
+export(float) var MAX_Y_SPEED: float = 50
 
 func _process(delta):
 	if Input.is_action_pressed("move_right"):
@@ -13,8 +11,8 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		move_vector += Vector2.LEFT * MOVE_SPEED
 	
-	move_vector.clamped(MAX_MOVE_SPEED)
+	move_vector.y -= GlobalVars.GRAVITY
+	move_vector.x = clamp(move_vector.x, -MAX_X_SPEED, MAX_X_SPEED)
+	move_vector.y = clamp(move_vector.y, -MAX_Y_SPEED, MAX_Y_SPEED)
 	
 	move_and_slide(move_vector)
-
-	
