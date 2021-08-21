f80c5c3434e87985b522137252c5ebac88fd7990
Phatarapran Saraluck <phatarapran7524@gmail.com>
2021-08-21 17:53:03 +0700

    Update endgame ui

--- scenes/uis/EndGameUI.gd
+++ scenes/uis/EndGameUI.gd
@@ -3,3 +3,7 @@ extends CanvasLayer
 func _on_RestartButton_pressed():
 	GameManager.emit_signal("game_start")
 	queue_free()
+
+
+func _on_MainMenuButton_pressed():
+	get_tree().change_scene("res://scenes/uis/MainMenuUI.tscn")
